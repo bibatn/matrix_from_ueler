731c95f718e5b8aeab319c59c2e3a52f8945a611
bibatn <habibulinmarat@icloud.com>
2023-01-17 15:07:59 +0300

    Правки.

--- calculate.py
+++ calculate.py
@@ -19,3 +19,42 @@ def from_euler(roll, pitch, yaw):
     HMatrix[2][1] = sr * cp
     HMatrix[2][2] = cr * cp
     return HMatrix
+    
+def Quaternion2EulerAngles(w, x, y, z):
+    PIby2 = (math.pi / 2)
+
+    # roll : rotation in x axis
+    roll_x = float(2 * (w * x + y * z))
+    roll_y = float(1 - 2 * (x * x + y * y))
+    roll = math.atan2(roll_x, roll_y);
+
+    # pitch : rotation in y axis
+    pitch_var = float(2 * (w * y - z * x))
+    if (abs(pitch_var) >= 1):
+	#In the event of out of range -> use 90 degrees
+        pitch = math.copysign(PIby2, pitch_var);
+    else:
+        pitch = math.asin(pitch_var);
+
+    # yaw : rotation in z-axis
+    yaw_x = 2 * (w * z + x * y);
+    yaw_y = 1 - 2 * (y * y + z * z);
+    yaw = math.atan2(yaw_x, yaw_y);
+
+    print("roll     ( radians )  : "+str(roll))
+    print("pitch    ( radians )  : "+str(pitch))
+    print("yaw      ( radians )  : "+str(yaw))
+
+    roll  = math.degrees(roll)
+    pitch = math.degrees(pitch)
+    yaw   = math.degrees(yaw)
+
+    print("roll     ( degrees )  : "+str(roll))
+    print("pitch    ( degrees )  : "+str(pitch))
+    print("yaw      ( degrees )  : "+str(yaw))
+
+    return roll, pitch, yaw
+    
+def q2matrix(w, x, y, z):
+  r, p, y = Quaternion2EulerAngles(w, x, y, z)
+  return from_euler(r, p, y)
